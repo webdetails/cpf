fdc648a45a9c4f16fb63ca6a38c80780e16859a8
Luis Silva <luis.silva@webdetails.pt>
2013-03-19 19:24:17 +0000

    modified 'processRequest' to show what the kettle transformation did (Testing). Protected this against empty .ktr files

--- src/pt/webdetails/cpk/elements/impl/KettleElementType.java
+++ src/pt/webdetails/cpk/elements/impl/KettleElementType.java
@@ -5,19 +5,32 @@
 package pt.webdetails.cpk.elements.impl;
 
 import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import net.sf.saxon.functions.Trace;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.pentaho.di.core.exception.KettleTransException;
 import org.pentaho.platform.api.engine.IParameterProvider;
 import pt.webdetails.cpf.SimpleContentGenerator;
 import pt.webdetails.cpk.elements.AbstractElementType;
 import pt.webdetails.cpk.elements.ElementInfo;
 import pt.webdetails.cpk.elements.IElement;
-
+import org.pentaho.di.core.Result;
+import org.pentaho.di.core.exception.KettleException;
+import org.pentaho.di.core.util.EnvUtil;
+import org.pentaho.di.job.Job;
+import org.pentaho.di.job.JobMeta;
+import org.pentaho.di.trans.Trans;
+import org.pentaho.di.trans.TransMeta;
+import org.pentaho.platform.engine.core.system.PentahoSystem;
 /**
  *
  * @author Pedro Alves<pedro.alves@webdetails.pt>
  */
 public class KettleElementType extends AbstractElementType {
+    
+    final String PLUGIN_NAME = "cvb";
 
     protected Log logger = LogFactory.getLog(this.getClass());
 
@@ -32,8 +45,42 @@ public class KettleElementType extends AbstractElementType {
 
     @Override
     public void processRequest(Map<String, IParameterProvider> parameterProviders, IElement element) {
-        logger.warn("Kettle's endpoint - not done yet, but it's good to be here!");
-        throw new UnsupportedOperationException("Not supported yet.");
+        
+        String kettlePath = PentahoSystem.getApplicationContext().getSolutionPath("system/" + PLUGIN_NAME + "/endpoints/kettle/");
+        String kettleFilename = "file";
+        String extension = ".ktr";
+        Result result = new Result();
+        
+        //TODO: Verify if it is a Transformation or a Job
+        //Just going to return a default transformation result for now (if there is one!)
+        
+        
+        
+        
+        try {
+            TransMeta transformationMeta = new TransMeta(kettlePath+kettleFilename+extension);
+            Trans transformation = new Trans(transformationMeta);
+            
+            transformation.beginProcessing();
+            transformation.waitUntilFinished();
+            result = transformation.getResult();
+            
+            
+            
+            
+          }
+          catch ( KettleException e ) {
+            // TODO Put your exception-handling code here.
+            logger.warn(e);
+            
+            if(e.toString().contains("Premature end of file")){
+                result.setLogText("The file ended prematurely. Please check the "+kettleFilename+extension+" file.");
+            }
+          }
+        
+        //Will use this to show the logText for now (Test purposes)
+        throw new UnsupportedOperationException(result.getLogText());
+        
     }
 
     @Override
