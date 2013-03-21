cf3edaaa1b370118cdc17455fd5682fcc2ee70e1
Pedro Alves <pedro.alves@webdetails.pt>
2013-03-21 16:04:55 +0000

    - Refactored KettleElementType code

--- src/pt/webdetails/cpk/elements/impl/KettleElementType.java
+++ src/pt/webdetails/cpk/elements/impl/KettleElementType.java
@@ -7,14 +7,8 @@ package pt.webdetails.cpk.elements.impl;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import javax.swing.JOptionPane;
-import net.sf.saxon.functions.Trace;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.pentaho.di.core.exception.KettleTransException;
-import org.pentaho.di.core.exception.KettleXMLException;
 import org.pentaho.platform.api.engine.IParameterProvider;
 import pt.webdetails.cpf.SimpleContentGenerator;
 import pt.webdetails.cpk.elements.AbstractElementType;
@@ -22,22 +16,22 @@ import pt.webdetails.cpk.elements.ElementInfo;
 import pt.webdetails.cpk.elements.IElement;
 import org.pentaho.di.core.Result;
 import org.pentaho.di.core.exception.KettleException;
-import org.pentaho.di.core.exception.KettleJobException;
-import org.pentaho.di.core.util.EnvUtil;
+import org.pentaho.di.core.exception.KettleXMLException;
+import org.pentaho.di.core.parameters.UnknownParamException;
 import org.pentaho.di.job.Job;
 import org.pentaho.di.job.JobMeta;
 import org.pentaho.di.trans.Trans;
 import org.pentaho.di.trans.TransMeta;
-import org.pentaho.platform.engine.core.system.PentahoSystem;
-import pt.webdetails.cpf.utils.PluginUtils;
+import pt.webdetails.cpf.Util;
+
 /**
  *
  * @author Pedro Alves<pedro.alves@webdetails.pt>
  */
 public class KettleElementType extends AbstractElementType {
-    
 
     protected Log logger = LogFactory.getLog(this.getClass());
+    private static final String PARAM_PREFIX = "param";
 
     public KettleElementType() {
     }
@@ -47,138 +41,134 @@ public class KettleElementType extends AbstractElementType {
         return "Kettle";
     }
 
-
     @Override
     public void processRequest(Map<String, IParameterProvider> parameterProviders, IElement element) {
-        
-        
+
+
         String kettlePath = element.getLocation();
         String kettleFilename = element.getName();
-        String extension = new String();
-        String operation = new String();
+        String extension;
+        String operation;
         String stepName = "OUTPUT";//Value by default
-        
-        Result result = new Result();
-        
-        logger.info("Kettle file path: "+kettlePath);
-        
+
+
+
+        logger.info("Processing request for: " + kettlePath);
+
         //This gets all the params inserted in the URL
         Iterator getCustomParams = parameterProviders.get("request").getParameterNames();
-        HashMap<String,String> customParams = new HashMap<String,String>();
-        String key = new String();
-        String value = new String();
-        String paramPrefix = "param";
-        
-        while(getCustomParams.hasNext()){
+        HashMap<String, String> customParams = new HashMap<String, String>();
+        String key;
+        String value;
+
+        while (getCustomParams.hasNext()) {
             key = getCustomParams.next().toString();
-            if(key.startsWith(paramPrefix)){
+            if (key.startsWith(PARAM_PREFIX)) {
                 value = parameterProviders.get("request").getParameter(key).toString();
                 customParams.put(key.substring(5), value);
-                logger.info("Argument '"+key.substring(5) +"' with value '"+value+"' stored on the map.");
-            }else{
-                logger.info("The parameter provided does not have a valid prefix. Try 'param"+key+"'.\n"
-                        + "The '"+key+" parameter will be stored without the 'param' prefix as it is just a signature.");
+                logger.debug("Argument '" + key.substring(5) + "' with value '" + value + "' stored on the map.");
+            } else {
+                // skip
             }
         }
-        
-        
-        String fileTypeInfo = "Kettle file type is: ";
-        if(kettlePath.endsWith(".ktr")){
+
+
+        if (kettlePath.endsWith(".ktr")) {
             operation = "Transformation";
-            logger.info(fileTypeInfo+operation);
             extension = ".ktr";
-        }else if(kettlePath.endsWith(".kjb")){
+        } else if (kettlePath.endsWith(".kjb")) {
             operation = "Job";
-            logger.info(fileTypeInfo+operation);
             extension = ".kjb";
-        }else{
-            logger.warn("File extension unknown!");
+        } else {
+            logger.warn("File extension unknown: " + kettlePath);
+            return;
         }
-       
+
+        Result result = null;
+
         //These conditions will treat the different types of kettle operations
-        if(operation.equalsIgnoreCase("transformation")){
-            try {
-                logger.info("Starting Kettle "+operation.toLowerCase()+"...");
 
-                TransMeta transformationMeta = new TransMeta(kettlePath);
-                Trans transformation = new Trans(transformationMeta);
+        try {
+            if (operation.equalsIgnoreCase("transformation")) {
+                result = executeTransformation(kettlePath, customParams);
+            } else {
+                result = executeJob(kettlePath, customParams);
+            }
 
-                /*
-                 * Loading parameters, if there are any.
-                 */
-                boolean parametersExist = false;
-                for(String arg : customParams.keySet()){
-                    parametersExist = true;
-                    transformation.setParameterValue(arg, customParams.get(arg));
-                    logger.info("'"+arg+"' with value '"+customParams.get(arg)+"' loaded into "+operation+" parameters.");
-                }
-                if(parametersExist){
-                    transformation.activateParameters();
-                    logger.info(operation+" parameters loaded with success!");
-                }else{
-                    logger.info("No parameters found for "+operation+".");
-                }
-                
-                
-                transformation.execute(null);
-                transformation.waitUntilFinished();
-                result = transformation.getResult();
+        } catch (KettleException e) {
 
+            logger.warn(" Error executing kettle file " + kettleFilename + ": " + Util.getExceptionDescription(e));
 
-
-                logger.info(operation+" complete");
-              }
-              catch ( KettleException e ) {
-                // TODO Put your exception-handling code here.
-                logger.warn(""+e);
-
-                if(e.toString().contains("Premature end of file")){
-                    result.setLogText("The file ended prematurely. Please check the "+kettleFilename+extension+" file.");
-                }else{
-                    throw new UnsupportedOperationException(e.toString());
-                }
-              }
-        }else if (operation.equalsIgnoreCase("job")){
-            try {
-                logger.info("Starting Kettle "+operation.toLowerCase()+"...");
-                
-                JobMeta jobMeta = new JobMeta(kettlePath, null);
-                Job job = new Job(null, jobMeta);
-                
-                /*
-                 * Loading parameters, if there are any.
-                 */
-                boolean parametersExist = false;
-                for(String arg : customParams.keySet()){
-                    parametersExist = true;
-                    job.setParameterValue(arg, customParams.get(arg));
-                    logger.info("'"+arg+"' with value '"+customParams.get(arg)+"' loaded into "+operation+" parameters.");
-                }
-                if(parametersExist){
-                    job.activateParameters();
-                    logger.info(operation+" parameters loaded with success!");
-                }else{
-                    logger.info("No parameters found for "+operation+".");
-                }
-                
-
-                job.start();
-                job.waitUntilFinished();
-                result = job.getResult();
-                
-                logger.info(operation+" complete");
-            } catch (KettleException ex) {
-                Logger.getLogger(KettleElementType.class.getName()).log(Level.SEVERE, null, ex);
+            if (e.toString().contains("Premature end of file")) {
+                result.setLogText("The file ended prematurely. Please check the " + kettleFilename + extension + " file.");
+            } else {
+                throw new UnsupportedOperationException(e.toString());
             }
         }
-        
-        //Will use this to show the result (Tests)
-        String resultMessage = new String();
-        
-        resultMessage = operation+" status:"+result;
-        
-        throw new UnsupportedOperationException(resultMessage);
-        
+
+
+        logger.info(operation + " " + kettlePath + " complete: " + result);
+
+
+    }
+
+    /**
+     * Executes a transformation
+     *
+     * @param kettlePath Path to the ktr
+     * @param customParams parameters to be passed to the transformation
+     * @return Result
+     * @throws KettleXMLException
+     * @throws UnknownParamException
+     * @throws KettleException
+     */
+    private Result executeTransformation(String kettlePath, HashMap<String, String> customParams) throws KettleXMLException, UnknownParamException, KettleException {
+
+        TransMeta transformationMeta = new TransMeta(kettlePath);
+        Trans transformation = new Trans(transformationMeta);
+        /*
+         * Loading parameters, if there are any.
+         */
+        if (customParams.size() > 0) {
+            for (String arg : customParams.keySet()) {
+                transformation.setParameterValue(arg, customParams.get(arg));
+            }
+            transformation.activateParameters();
+
+        }
+        transformation.execute(null);
+        transformation.waitUntilFinished();
+        return transformation.getResult();
+    }
+
+    /**
+     *
+     * @param kettlePath Path to the kjb
+     * @param customParams parameters to be passed to the job
+     * @return
+     * @throws UnknownParamException
+     * @throws KettleException
+     * @throws KettleXMLException
+     */
+    private Result executeJob(String kettlePath, HashMap<String, String> customParams) throws UnknownParamException, KettleException, KettleXMLException {
+
+
+        JobMeta jobMeta = new JobMeta(kettlePath, null);
+        Job job = new Job(null, jobMeta);
+
+        /*
+         * Loading parameters, if there are any.
+         */
+        if (customParams.size() > 0) {
+            for (String arg : customParams.keySet()) {
+                job.setParameterValue(arg, customParams.get(arg));
+            }
+            job.activateParameters();
+
+        }
+        job.start();
+        job.waitUntilFinished();
+        return job.getResult();
     }
 
     @Override
