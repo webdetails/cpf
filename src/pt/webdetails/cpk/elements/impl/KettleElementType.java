d6328c0e06946516e6c68a306b6b5baaeb519971
Luis Silva <luis.silva@webdetails.pt>
2013-03-22 15:20:37 +0000

    Created class inside KettleElementType.java that stores the rows as they are read. Created new ConcurrentHashMap to store the rows of a given kettle. Key=kettlePath and Value=KettleOutput

--- src/pt/webdetails/cpk/elements/impl/KettleElementType.java
+++ src/pt/webdetails/cpk/elements/impl/KettleElementType.java
@@ -4,12 +4,14 @@
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 package pt.webdetails.cpk.elements.impl;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.json.JSONArray;
 import org.pentaho.platform.api.engine.IParameterProvider;
 import pt.webdetails.cpf.SimpleContentGenerator;
 import pt.webdetails.cpk.elements.AbstractElementType;
@@ -21,6 +23,7 @@ import org.pentaho.di.core.exception.KettleStepException;
 import org.pentaho.di.core.exception.KettleXMLException;
 import org.pentaho.di.core.parameters.UnknownParamException;
 import org.pentaho.di.core.row.RowMetaInterface;
+import org.pentaho.di.core.util.AddClosureArrayList;
 import org.pentaho.di.job.Job;
 import org.pentaho.di.job.JobMeta;
 import org.pentaho.di.trans.Trans;
@@ -38,8 +41,9 @@ public class KettleElementType extends AbstractElementType {
     protected Log logger = LogFactory.getLog(this.getClass());
     private static final String PARAM_PREFIX = "param";
     private String stepName = "OUTPUT";//Value by default
-    private static ConcurrentHashMap<String,TransMeta> transMetaStorage = new ConcurrentHashMap<String, TransMeta>();//Stores the metadata of the ktr files.
-    private static ConcurrentHashMap<String, JobMeta> jobMetaStorage = new ConcurrentHashMap<String, JobMeta>();//Stores the metadata of the kjb files.
+    private static ConcurrentHashMap<String,TransMeta> transMetaStorage = new ConcurrentHashMap<String, TransMeta>();//Stores the metadata of the ktr files. [Key=path]&[Value=transMeta]
+    private static ConcurrentHashMap<String, JobMeta> jobMetaStorage = new ConcurrentHashMap<String, JobMeta>();//Stores the metadata of the kjb files. [Key=path]&[Value=jobMeta]
+    private static ConcurrentHashMap<String, KettleOutput> kettleOutputStorage = new ConcurrentHashMap<String, KettleOutput>();//Stores the kettle outputs for each of the kettle files ran. [Key=path]&[Value=kettleOutput]
 
     public KettleElementType() {
     }
@@ -136,9 +140,15 @@ public class KettleElementType extends AbstractElementType {
      * @throws UnknownParamException
      * @throws KettleException
      */
-    private Result executeTransformation(String kettlePath, HashMap<String, String> customParams) throws KettleXMLException, UnknownParamException, KettleException {
+    private Result executeTransformation(final String kettlePath, HashMap<String, String> customParams) throws KettleXMLException, UnknownParamException, KettleException {
 
+        
+        KettleOutput kettleOutput = new KettleOutput();
+        kettleOutputStorage.put(kettlePath, kettleOutput);
+        
+        
         TransMeta transformationMeta = new TransMeta();
+        
         if(transMetaStorage.containsKey(kettlePath)){
             logger.debug("Existent metadata found for "+kettlePath);
             transformationMeta = transMetaStorage.get(kettlePath);
@@ -170,25 +180,24 @@ public class KettleElementType extends AbstractElementType {
         transformation.prepareExecution(null);
         
         StepInterface step = transformation.findRunThread(stepName);
-        
-        if(transformation.getResult().getRows()!=null){
-            step.addRowListener(new RowAdapter(){
-                @Override
-                public void rowReadEvent(RowMetaInterface rowMeta, Object[] row) throws KettleStepException {
-                   // Here you get the rows as they are read by the step     
-                }
-
-                @Override
-                public void rowWrittenEvent(RowMetaInterface rowMeta, Object[] row) throws KettleStepException {
-                   // Here you get the rows as they are written by the step
-                }
-            }
-            );
-        }else{
-            
-        }
-        
         transformation.startThreads();
+        
+        
+        step.addRowListener(new RowAdapter(){
+            @Override
+            public void rowReadEvent(RowMetaInterface rowMeta, Object[] row) throws KettleStepException {
+                kettleOutputStorage.get(kettlePath).storeRow(row);
+            }
+
+            @Override
+            public void rowWrittenEvent(RowMetaInterface rowMeta, Object[] row) throws KettleStepException {
+               // TODO
+            }
+        }
+        );
+       
+        
+        
         transformation.waitUntilFinished();
         return transformation.getResult();
     }
@@ -207,6 +216,7 @@ public class KettleElementType extends AbstractElementType {
 
 
         JobMeta jobMeta = new JobMeta();
+        
         
         if(jobMetaStorage.containsKey(kettlePath)){
             logger.debug("Existent metadata found for "+kettlePath);
@@ -238,5 +248,46 @@ public class KettleElementType extends AbstractElementType {
     @Override
     protected ElementInfo createElementInfo() {
         return new ElementInfo(SimpleContentGenerator.MimeType.JSON, 0);
+    }
+    
+    private class KettleOutput{
+        ArrayList<Object[]> rows;
+        ArrayList<RowMetaInterface> rowsMeta;
+        
+        KettleOutput(){
+            init();
+        }
+        
+        private void init(){
+            rows = new ArrayList<Object[]>();
+            rowsMeta = new ArrayList<RowMetaInterface>();
+        }
+        
+        public void storeRow(Object[] row){
+            rows.add(row);
+        }
+        
+        public void storeRow(Object[] row, RowMetaInterface rowMeta){
+            rows.add(row);
+            rowsMeta.add(rowMeta);
+        }
+        
+        public ArrayList<Object[]> getRows(){
+            return rows;
+        }
+        
+        public ArrayList<RowMetaInterface> getRowsMeta(){
+            return rowsMeta;
+        }
+        
+        public JSONArray getRowsJSON(){
+            return null;
+        }
+        
+        
+        /*
+         * To implement later on...
+         * public void removeRow(){}
+         */
     }
 }
