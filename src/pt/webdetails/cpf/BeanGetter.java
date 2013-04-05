cdf1edcf0290af52a4ad96a606ecbd007dce51ef
Pedro Vale <pedro.vale@webdetails.pt>
2013-04-05 13:15:09 +0100

    format

--- src/pt/webdetails/cpf/BeanGetter.java
+++ src/pt/webdetails/cpf/BeanGetter.java
@@ -15,73 +15,63 @@ import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.FileSystemXmlApplicationContext;
 
 public class BeanGetter {
-  
-  
+
   private static Log logger = LogFactory.getLog(BeanGetter.class);
   private static BeanGetter _instance;
-  protected ConfigurableApplicationContext context;  
-  
-  
+  protected ConfigurableApplicationContext context;
+
   public static BeanGetter getInstance(String pluginName) {
     if (_instance == null) {
       _instance = new BeanGetter(pluginName);
     }
-    
+
     return _instance;
   }
-  
-  
-  
 
-  
   private ConfigurableApplicationContext getSpringBeanFactory(String pluginName) {
 
-   final PluginClassLoader loader = (PluginClassLoader) PentahoSystem.get(IPluginManager.class)
-                .getClassLoader(pluginName);
-        logger.warn(loader.getPluginDir());
-        File f = new File(loader.getPluginDir(), "plugin.spring.xml"); //$NON-NLS-1$
-        if (f.exists()) {
-            logger.debug("Found plugin spring file @ " + f.getAbsolutePath()); //$NON-NLS-1$
-            ConfigurableApplicationContext context = new FileSystemXmlApplicationContext(
-                    "file:" + f.getAbsolutePath()) { //$NON-NLS-1$
-                @Override
-                protected void initBeanDefinitionReader(
-                        XmlBeanDefinitionReader beanDefinitionReader) {
+    final PluginClassLoader loader = (PluginClassLoader) PentahoSystem.get(IPluginManager.class).getClassLoader(pluginName);
+    logger.warn(loader.getPluginDir());
+    File f = new File(loader.getPluginDir(), "plugin.spring.xml"); //$NON-NLS-1$
+    if (f.exists()) {
+      logger.debug("Found plugin spring file @ " + f.getAbsolutePath()); //$NON-NLS-1$
+      ConfigurableApplicationContext context = new FileSystemXmlApplicationContext(
+              "file:" + f.getAbsolutePath()) { //$NON-NLS-1$
 
-                    beanDefinitionReader.setBeanClassLoader(loader);
-                }
+        @Override
+        protected void initBeanDefinitionReader(
+                XmlBeanDefinitionReader beanDefinitionReader) {
 
-                @Override
-                protected void prepareBeanFactory(
-                        ConfigurableListableBeanFactory clBeanFactory) {
-                    super.prepareBeanFactory(clBeanFactory);
-                    clBeanFactory.setBeanClassLoader(loader);
-                }
-
-                /**
-                 * Critically important to override this and return the desired
-                 * CL
-				 *
-                 */
-                @Override
-                public ClassLoader getClassLoader() {
-                    return loader;
-                }
-            };
-            return context;
+          beanDefinitionReader.setBeanClassLoader(loader);
         }
-        throw new IllegalStateException("no plugin.spring.xml file found"); //$NON-NLS-1        
-  }  
-  
-  
-  
-  private BeanGetter(String pluginName) {
-    context = getSpringBeanFactory(pluginName);    
+
+        @Override
+        protected void prepareBeanFactory(
+                ConfigurableListableBeanFactory clBeanFactory) {
+          super.prepareBeanFactory(clBeanFactory);
+          clBeanFactory.setBeanClassLoader(loader);
+        }
+
+        /**
+         * Critically important to override this and return the desired
+         * CL
+         *
+         */
+        @Override
+        public ClassLoader getClassLoader() {
+          return loader;
+        }
+      };
+      return context;
+    }
+    throw new IllegalStateException("no plugin.spring.xml file found"); //$NON-NLS-1        
   }
-  
-  
+
+  private BeanGetter(String pluginName) {
+    context = getSpringBeanFactory(pluginName);
+  }
+
   public Object getBean(String id) {
     return context.getBean(id);
   }
-  
 }
