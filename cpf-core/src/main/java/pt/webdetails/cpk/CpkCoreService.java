264bc219108c0f4f471244c8e0ad6587043df869
Mark Reid <mreid@mozilla.com>
2013-05-17 12:35:46 +0100

    Minor cleanup (no more warnings)

--- cpf-core/src/main/java/pt/webdetails/cpk/CpkCoreService.java
+++ cpf-core/src/main/java/pt/webdetails/cpk/CpkCoreService.java
@@ -15,32 +15,25 @@ import org.dom4j.DocumentException;
 import pt.webdetails.cpf.RestRequestHandler;
 import pt.webdetails.cpf.Router;
 import pt.webdetails.cpf.http.ICommonParameterProvider;
-import pt.webdetails.cpf.repository.IRepositoryAccess;
 import pt.webdetails.cpf.utils.IPluginUtils;
 import pt.webdetails.cpk.elements.IElement;
 import pt.webdetails.cpk.security.IAccessControl;
-
 
 /**
  *
  * @author joao
  */
 public class CpkCoreService {
-
-
-    private static final long serialVersionUID = 1L;
     public static final String CDW_EXTENSION = ".cdw";
     public static final String PLUGIN_NAME = "cpk";
     private static final String ENCODING = "UTF-8";
     protected CpkEngine cpkEngine;
-    private IRepositoryAccess repAccess;
     private static final Logger logger = Logger.getLogger(CpkCoreService.class.getName());
     protected ICpkEnvironment cpkEnvironment;
 
     public CpkCoreService(ICpkEnvironment environment){
         this.cpkEnvironment = environment;
     }
-    //public CpkCoreService(){}
 
     private CpkEngine getCpkEngine() {
         if (cpkEngine == null) {
@@ -54,10 +47,9 @@ public class CpkCoreService {
 
         //Make sure the instance is first set so we have pluginUtils
         CpkEngine engine = getCpkEngine();
-
         IAccessControl accessControl = cpkEnvironment.getAccessControl();
 
-        logger.log(Level.WARNING,"Creating content");//switched from debug("Creating content")
+        logger.log(Level.WARNING,"Creating content");
 
         // Get the path, remove leading slash
         IPluginUtils pluginUtils = cpkEnvironment.getPluginUtils();
@@ -65,7 +57,6 @@ public class CpkCoreService {
         IElement element = null;
 
         if (path == null || path.equals("/")) {
-
             String url = engine.getDefaultElement().getId().toLowerCase();
             if (path == null) {
                 // We need to put the http redirection on the right level
@@ -88,29 +79,23 @@ public class CpkCoreService {
         }
     }
 
+    // alias to refresh
     public void reload(OutputStream out,Map<String,ICommonParameterProvider> parameterProviders) throws DocumentException, IOException {
-
-        // alias to refresh
         refresh(out, parameterProviders);
     }
 
-
     public void refresh(OutputStream out, Map<String,ICommonParameterProvider> parameterProviders) throws DocumentException, IOException {
         IAccessControl accessControl = cpkEnvironment.getAccessControl();
-        if(accessControl.isAdmin()){
+        if (accessControl.isAdmin()) {
             logger.info("Refreshing CPK plugin " + getPluginName());
             getCpkEngine().reload();
             status(out, parameterProviders);
-        }else{
+        } else {
             accessControl.throwAccessDenied(parameterProviders);
         }
-
-
     }
 
-
     public void status(OutputStream out, Map<String,ICommonParameterProvider> parameterProviders) throws DocumentException, IOException {
-
         logger.info("Showing status for CPK plugin " + getPluginName());
 
         // Only set the headers if we have access to the response (via parameterProviders).
@@ -118,9 +103,7 @@ public class CpkCoreService {
             cpkEnvironment.getPluginUtils().setResponseHeaders(parameterProviders, "text/plain");
         }
         out.write(getCpkEngine().getStatus().getBytes("UTF-8"));
-
     }
-
 
     public boolean hasElement(String elementId) {
         TreeMap<String, IElement> elementsMap = getCpkEngine().getElementsMap();
@@ -129,7 +112,6 @@ public class CpkCoreService {
 
     public IElement[] getElements() {
         IElement[] elements = new IElement[]{};
-
         CpkEngine engine = getCpkEngine();
 
         if (engine != null) {
@@ -147,25 +129,19 @@ public class CpkCoreService {
         return elements;
     }
 
-    public void getElementsList(OutputStream out){
-        try {
-            out.write(getCpkEngine().getElementsJson().getBytes(ENCODING));
-        } catch (IOException ex) {
-            Logger.getLogger(CpkCoreService.class.getName()).log(Level.SEVERE, null, ex);
-        }
+    public void getElementsList(OutputStream out) {
+        writeMessage(out, getCpkEngine().getElementsJson());
     }
 
-
     public String getPluginName() {
-
       return cpkEnvironment.getPluginName();
     }
 
-    private void writeMessage(OutputStream out, String message){
+    private void writeMessage(OutputStream out, String message) {
         try {
             out.write(message.getBytes(ENCODING));
         } catch (IOException ex) {
-            Logger.getLogger(CpkCoreService.class.getName()).log(Level.SEVERE, null, ex);
+            logger.log(Level.SEVERE, "Error writing message", ex);
         }
     }
 
