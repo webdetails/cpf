baf24b1a0e6154d13cbd206e5e27f89e82d665f3
Pedro Teixeira <pedrofvteixeira@gmail.com>
2013-11-25 18:48:53 +0000

    moved XmlDom4jUtils from cpf-pentaho-base to cpf-core; for that, I replaced org.pentaho.platform.util.xml.dom4j.XmlDom4JHelper with org.dom4j.io.SAXReader; applied code-style check

---
+++ cpf-core/src/pt/webdetails/cpf/utils/XmlDom4JUtils.java
@@ -0,0 +1,45 @@
+package pt.webdetails.cpf.utils;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.dom4j.Document;
+import org.dom4j.io.SAXReader;
+
+import pt.webdetails.cpf.repository.api.IBasicFile;
+import pt.webdetails.cpf.repository.api.IReadAccess;
+
+public class XmlDom4JUtils {
+
+  protected static Log logger = LogFactory.getLog( XmlDom4JUtils.class );
+
+  public static Document getDocumentFromStream( InputStream is ) {
+
+    if ( is == null ) {
+      return null;
+    }
+
+    try {
+      return new SAXReader().read( is );
+    } catch ( Exception ex ) {
+      logger.error( ex );
+    } finally {
+      IOUtils.closeQuietly( is );
+    }
+    return null;
+  }
+
+  public static Document getDocumentFromFile( final IBasicFile file ) throws IOException {
+    return ( file != null ) ? getDocumentFromStream( file.getContents() ) : null;
+  }
+
+  public static Document getDocumentFromFile( final IReadAccess access, final String filePath ) throws IOException {
+    if ( access != null && filePath != null && access.fileExists( filePath ) ) {
+      return getDocumentFromFile( access.fetchFile( filePath ) );
+    }
+    return null;
+  }
+}
