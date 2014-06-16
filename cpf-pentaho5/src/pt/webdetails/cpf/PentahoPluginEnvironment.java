4aba855ba80ea442f65f1f438109f754bce1b4b1
Diogo Mariano <diogo.mariano@webdetails.pt>
2014-06-16 18:52:39 +0100

    [CDE-269] - wip added root methods

--- cpf-pentaho5/src/pt/webdetails/cpf/PentahoPluginEnvironment.java
+++ cpf-pentaho5/src/pt/webdetails/cpf/PentahoPluginEnvironment.java
@@ -2,17 +2,16 @@ package pt.webdetails.cpf;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.pentaho.platform.engine.core.system.PentahoRequestContextHolder;
+import org.pentaho.platform.engine.core.system.PentahoSystem;
 import org.pentaho.platform.engine.core.system.PentahoSessionHolder;
+import org.pentaho.platform.engine.core.system.PentahoRequestContextHolder;
 
 import pt.webdetails.cpf.api.IContentAccessFactoryExtended;
 import pt.webdetails.cpf.api.IUserContentAccessExtended;
 import pt.webdetails.cpf.context.api.IUrlProvider;
-import pt.webdetails.cpf.messaging.BeanyPluginCall;
 import pt.webdetails.cpf.plugincall.api.IPluginCall;
 import pt.webdetails.cpf.repository.api.IRWAccess;
 import pt.webdetails.cpf.repository.api.IReadAccess;
-import pt.webdetails.cpf.repository.api.IUserContentAccess;
 import pt.webdetails.cpf.repository.pentaho.unified.PluginRepositoryResourceAccess;
 import pt.webdetails.cpf.repository.pentaho.unified.UserContentRepositoryAccess;
 import pt.webdetails.cpf.repository.util.RepositoryHelper;
@@ -21,46 +20,50 @@ import pt.webdetails.cpf.repository.util.RepositoryHelper;
 public class PentahoPluginEnvironment extends PentahoBasePluginEnvironment implements IContentAccessFactoryExtended {
 
   private static PentahoPluginEnvironment instance = new PentahoPluginEnvironment();
-  static {
-    PluginEnvironment.init( instance );
-  }
-  private static Log logger = LogFactory.getLog(PentahoPluginEnvironment.class);
 
   static {
     PluginEnvironment.init( instance );
   }
 
-  protected PentahoPluginEnvironment() {}
+  private static Log logger = LogFactory.getLog( PentahoPluginEnvironment.class );
+
+  static {
+    PluginEnvironment.init( instance );
+  }
+
+  protected PentahoPluginEnvironment() {
+  }
 
   public static PentahoPluginEnvironment getInstance() {
     return instance;
   }
 
   public IContentAccessFactoryExtended getContentAccessFactory() {
-	return this;
-  }
-  
-  @Override
-  public IUserContentAccessExtended getUserContentAccess(String basePath) {
-    return new UserContentRepositoryAccess( PentahoSessionHolder.getSession(), basePath);
+    return this;
   }
 
   @Override
-  public IReadAccess getPluginRepositoryReader(String basePath) {
-    basePath = RepositoryHelper.appendPath(getPluginRepositoryDir(), basePath);
-    return new PluginRepositoryResourceAccess(basePath);
+  public IUserContentAccessExtended getUserContentAccess( String basePath ) {
+    return new UserContentRepositoryAccess( PentahoSessionHolder.getSession(), basePath );
   }
 
   @Override
-  public IRWAccess getPluginRepositoryWriter(String basePath) {
-    basePath = RepositoryHelper.appendPath(getPluginRepositoryDir(), basePath);
-    return new PluginRepositoryResourceAccess(basePath);
+  public IReadAccess getPluginRepositoryReader( String basePath ) {
+    basePath = RepositoryHelper.appendPath( getPluginRepositoryDir(), basePath );
+    return new PluginRepositoryResourceAccess( basePath );
   }
 
+  @Override
+  public IRWAccess getPluginRepositoryWriter( String basePath ) {
+    basePath = RepositoryHelper.appendPath( getPluginRepositoryDir(), basePath );
+    return new PluginRepositoryResourceAccess( basePath );
+  }
 
   @Override
   public IUrlProvider getUrlProvider() {
     return new IUrlProvider() {
+
+      private static final String REPOS = "api/repos/";
 
       @Override
       public String getPluginBaseUrl( String pluginId ) {
@@ -74,7 +77,7 @@ public class PentahoPluginEnvironment extends PentahoBasePluginEnvironment imple
 
       @Override
       public String getPluginStaticBaseUrl( String pluginId ) {
-        return Util.joinPath( getWebappContextPath(), "api/repos/", pluginId ) + "/";
+        return Util.joinPath( getWebappContextPath(), REPOS, pluginId ) + "/";
       }
 
       @Override
@@ -83,19 +86,28 @@ public class PentahoPluginEnvironment extends PentahoBasePluginEnvironment imple
       }
 
       @Override
-      public String getRepositoryUrl(String fullPath) {
+      public String getRepositoryUrl( String fullPath ) {
         String colonPath = fullPath.replaceAll( "/", ":" );
-        return Util.joinPath( getWebappContextPath(), "/api/repos/", colonPath, "/content" );
+        return Util.joinPath( getWebappContextPath(), REPOS, colonPath, "/content" );
       }
 
-      private String getWebappContextPath() { //TODO: better alternative
+      @Override
+      public String getWebappContextPath() {
         return PentahoRequestContextHolder.getRequestContext().getContextPath();
+      }
+
+      @Override
+      public String getWebappContextRoot() {
+        String url = PentahoSystem.getApplicationContext().getFullyQualifiedServerURL(),
+          webappName = getWebappContextPath();
+
+        return url.substring( 0, url.length() - webappName.length() + 1 );
       }
     };
   }
 
   public IPluginCall getPluginCall( String pluginId, String servicePath, String method ) {
-    return new InterPluginCall( new InterPluginCall.Plugin(pluginId), servicePath, method );
+    return new InterPluginCall( new InterPluginCall.Plugin( pluginId ), servicePath, method );
   }
 
 }
