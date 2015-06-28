b3f50e0c0edf91d5408a5470f9f6b1e03fed38be
Pedro Teixeira <pedrofvteixeira@gmail.com>
2015-06-28 21:35:23 +0100

    [CDA-154] Promote CoreBeanFactory to CPF 	- moving CoreBeanFactory and spring dependencies to cpf-core 	- CoreBeanFactory was made abstract; other plugins will simply need to implement abstract method getSpringXMLFilename() 	- other plugins will stop using their own CoreBeanFactory and leverage on this one 	- other plugins' dependency footprint will be reduced, as many will no longer need to include spring.framework

---
+++ cpf-core/test-src/pt/webdetails/cpf/bean/BeanFactoryTest.java
@@ -0,0 +1,83 @@
+/*!
+ * Copyright 2002 - 2015 Webdetails, a Pentaho company.  All rights reserved.
+ *
+ * This software was developed by Webdetails and is provided under the terms
+ * of the Mozilla Public License, Version 2.0, or any later version. You may not use
+ * this file except in compliance with the license. If you need a copy of the license,
+ * please go to  http://mozilla.org/MPL/2.0/. The Initial Developer is Webdetails.
+ *
+ * Software distributed under the Mozilla Public License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or  implied. Please refer to
+ * the license for the specific language governing your rights and limitations.
+ */
+package pt.webdetails.cpf.bean;
+
+import junit.framework.Assert;
+import junit.framework.TestCase;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import pt.webdetails.cpf.bean.IDummyBean;
+
+public class BeanFactoryTest extends TestCase {
+
+  /*
+   * We tend to use BeanFactory class to load spring xml files that are
+   * in the classloader's base path.
+   *
+   * But in unit testing, things differ: the test class
+   * is in a /test-src dir, but it gets copied into /bin/test/classes
+   *
+   * So we count from that base dir onwards
+   */
+
+  private final String SPRING_XML_FILE = "./pt/webdetails/cpf/bean/test.spring.xml";
+
+  IDummyBean dummyBean;
+  IBeanFactory factory;
+
+  @Before
+  public void setUp() {
+
+    factory = new AbstractBeanFactory() {
+      @Override public String getSpringXMLFilename() { return SPRING_XML_FILE; }
+    };
+  }
+
+  @Test
+  public void testSpringXmlFileFound() {
+
+    assertNotNull( factory );
+
+    assertFalse( factory.containsBean( "IBogusBean" ) );
+
+    assertTrue( factory.containsBean( IDummyBean.class.getSimpleName() ) );
+
+  }
+
+  @Test
+  public void testBeanLoadingOK() {
+
+    assertNotNull( factory );
+
+    try {
+
+      dummyBean = ( IDummyBean ) factory.getBean( IDummyBean.class.getSimpleName() );
+
+    } catch( Throwable t ) {
+      Assert.fail();
+    }
+
+    assertNotNull( dummyBean );
+
+    assertTrue( dummyBean.isBeanOK() );
+  }
+
+  @After
+  public void tearDown(){
+
+    factory = null;
+    dummyBean = null;
+  }
+}
