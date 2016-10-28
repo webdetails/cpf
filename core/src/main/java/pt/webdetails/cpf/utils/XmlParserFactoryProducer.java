f2cdf0f80f8e6a313dc2367cce0158616c7287fd
Yury Bakhmutski <YURY_BAKHMUTSKI@epam.com>
2016-10-28 15:13:09 +0300

    [PPP-3506] - changes according to the checkstyle

--- core/src/main/java/pt/webdetails/cpf/utils/XmlParserFactoryProducer.java
+++ core/src/main/java/pt/webdetails/cpf/utils/XmlParserFactoryProducer.java
@@ -21,32 +21,32 @@ import org.xml.sax.SAXException;
 import javax.xml.XMLConstants;
 
 public class XmlParserFactoryProducer {
-    private static final Log logger = LogFactory.getLog( XmlParserFactoryProducer.class );
+  private static final Log logger = LogFactory.getLog( XmlParserFactoryProducer.class );
 
-    /**
-     * Creates an instance of {@link SAXReader} class
-     * with features that prevent from some XXE attacks (e.g. XML bomb)
-     * See PPP-3506 for more details.
-     * See also https://www.owasp.org/index.php/XML_External_Entity_(XXE)_Prevention_Cheat_Sheet
-     *
-     * @param resolver Is {@link EntityResolver} or null
-     * @return {@link SAXReader}
-     */
-    public static SAXReader getSAXReader( final EntityResolver resolver ) {
-        SAXReader reader = new SAXReader();
-        if ( resolver != null ) {
-            reader.setEntityResolver( resolver );
-        }
-        try {
-            reader.setFeature( XMLConstants.FEATURE_SECURE_PROCESSING, true );
-            reader.setFeature( "http://xml.org/sax/features/external-general-entities", false );
-            reader.setFeature( "http://xml.org/sax/features/external-parameter-entities", false );
-            reader.setFeature( "http://apache.org/xml/features/nonvalidating/load-external-dtd", false );
-        } catch ( SAXException e ) {
-            logger.error( "Some parser properties are not supported." );
-        }
-        reader.setIncludeExternalDTDDeclarations( false );
-        reader.setIncludeInternalDTDDeclarations( false );
-        return reader;
+  /**
+   * Creates an instance of {@link SAXReader} class
+   * with features that prevent from some XXE attacks (e.g. XML bomb)
+   * See PPP-3506 for more details.
+   * See also https://www.owasp.org/index.php/XML_External_Entity_(XXE)_Prevention_Cheat_Sheet
+   *
+   * @param resolver Is {@link EntityResolver} or null
+   * @return {@link SAXReader}
+   */
+  public static SAXReader getSAXReader( final EntityResolver resolver ) {
+    SAXReader reader = new SAXReader();
+    if ( resolver != null ) {
+      reader.setEntityResolver( resolver );
     }
+    try {
+      reader.setFeature( XMLConstants.FEATURE_SECURE_PROCESSING, true );
+      reader.setFeature( "http://xml.org/sax/features/external-general-entities", false );
+      reader.setFeature( "http://xml.org/sax/features/external-parameter-entities", false );
+      reader.setFeature( "http://apache.org/xml/features/nonvalidating/load-external-dtd", false );
+    } catch ( SAXException e ) {
+      logger.error( "Some parser properties are not supported." );
+    }
+    reader.setIncludeExternalDTDDeclarations( false );
+    reader.setIncludeInternalDTDDeclarations( false );
+    return reader;
+  }
 }
