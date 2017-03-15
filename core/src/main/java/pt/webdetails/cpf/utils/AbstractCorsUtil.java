bd090359afadece18c538b4ccc79df39c9151cec
Egor Zhuk <Yahor_Zhuk@epam.com>
2017-03-15 16:35:19 +0300

    [CDA-211]-Abstract class to implement CorsUtil in plugins

---
+++ core/src/main/java/pt/webdetails/cpf/utils/AbstractCorsUtil.java
@@ -0,0 +1,99 @@
+/*!
+ * Copyright 2002 - 2017 Webdetails, a Pentaho company. All rights reserved.
+ *
+ * This software was developed by Webdetails and is provided under the terms
+ * of the Mozilla Public License, Version 2.0, or any later version. You may not use
+ * this file except in compliance with the license. If you need a copy of the license,
+ * please go to http://mozilla.org/MPL/2.0/. The Initial Developer is Webdetails.
+ *
+ * Software distributed under the Mozilla Public License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. Please refer to
+ * the license for the specific language governing your rights and limitations.
+ */
+
+package pt.webdetails.cpf.utils;
+
+import org.apache.commons.lang.StringUtils;
+import org.springframework.util.CollectionUtils;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Collection;
+
+/**
+ * An abstract class to provide a carcass for a CORS implementation in plugins.
+ * The idea is in populating an HTTP response with a CORS headers, allowing access
+ * from a domain specified by a user in an {@link #ORIGIN} header if it is also listed
+ * in an whitelist {@link #getDomainWhitelist()}.
+ */
+public abstract class AbstractCorsUtil {
+
+  private static final String TRUE = "true";
+  private static final String ORIGIN = "ORIGIN";
+  private static final String ACCESS_CONTROL_ALLOW_ORIGIN = "Access-Control-Allow-Origin";
+  private static final String ACCESS_CONTROL_ALLOW_CREDENTIALS = "Access-Control-Allow-Credentials";
+
+  /**
+   * The value is assumed to be retrieved from a plugin configuration file.
+   * In the default implementation we use a property with a name <i>allow-cross-domain-resources</i>,
+   * so it is a good idea to use the same name in your own implementation.
+   *
+   * @return {@link java.lang.Boolean#TRUE} if cross-domain calls are allowed
+   */
+  protected abstract boolean isCorsAllowed();
+
+  /**
+   * The values are assumed to be retrieved from a plugin configuration file.
+   * In the default implementation we use a property with a name <i>cross-domain-resources-whitelist</i>,
+   * so it is a good idea to use the same name in your own implementation
+   *
+   * @return collection of allowed domains
+   */
+  protected abstract Collection<String> getDomainWhitelist();
+
+  /**
+   * Sets CORS headers to the response if the domain provided by a user is in a whitelist
+   *
+   * @param request  a request, nothing special
+   * @param response a response, nothing special
+   */
+  public void setCorsHeaders( HttpServletRequest request, HttpServletResponse response ) {
+    if ( isCorsAllowed() ) {
+      final String origin = request.getHeader( ORIGIN );
+      if ( isDomainAllowed( origin ) ) {
+        response.setHeader( ACCESS_CONTROL_ALLOW_ORIGIN, origin );
+        response.setHeader( ACCESS_CONTROL_ALLOW_CREDENTIALS, TRUE );
+      }
+    }
+  }
+
+
+  /**
+   * Checks if a domain is a whitelist provided by a {@link #getDomainWhitelist()}.
+   *
+   * @param untrustedDomain a value provided by a user, not safe
+   * @return If the parameter value is empty, not present in a whitelist, or there is no values in a list {@link
+   * java.lang.Boolean#FALSE} is returned. Otherwise {@link java.lang.Boolean#TRUE is returned}
+   */
+  private boolean isDomainAllowed( final String untrustedDomain ) {
+
+    if ( StringUtils.isBlank( untrustedDomain ) ) {
+      return false;
+    }
+
+    final Collection<String> domainWhitelist = getDomainWhitelist();
+
+    if ( CollectionUtils.isEmpty( domainWhitelist ) ) {
+      return false;
+    }
+
+    for ( String domain : domainWhitelist ) {
+      if ( StringUtils.equalsIgnoreCase( domain, untrustedDomain ) ) {
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+}
---
+++ core/src/main/java/pt/webdetails/cpf/utils/CsvUtil.java
@@ -0,0 +1,43 @@
+/*!
+ * Copyright 2002 - 2017 Webdetails, a Pentaho company. All rights reserved.
+ *
+ * This software was developed by Webdetails and is provided under the terms
+ * of the Mozilla Public License, Version 2.0, or any later version. You may not use
+ * this file except in compliance with the license. If you need a copy of the license,
+ * please go to http://mozilla.org/MPL/2.0/. The Initial Developer is Webdetails.
+ *
+ * Software distributed under the Mozilla Public License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. Please refer to
+ * the license for the specific language governing your rights and limitations.
+ */
+
+package pt.webdetails.cpf.utils;
+
+import org.apache.commons.lang.StringUtils;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashSet;
+
+/**
+ * A simple utility class for operations with comma-separated values
+ */
+public class CsvUtil {
+  private static final String COMMA = ",";
+
+  /**
+   * An utility method used in the default implementations.
+   * Is intentionally left here in order not to implement it in each plugin.
+   *
+   * @param csvString a comma separated string
+   * @return a collection of strings, never a null
+   */
+  public static Collection<String> parseCsvString( final String csvString ) {
+    if ( StringUtils.isNotBlank( csvString ) ) {
+      final String[] strings = csvString.split( COMMA );
+      return new HashSet<>( Arrays.asList( strings ) );
+    }
+    return Collections.emptyList();
+  }
+}
