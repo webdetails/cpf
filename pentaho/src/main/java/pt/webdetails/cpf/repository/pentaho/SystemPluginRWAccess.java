2d33a7fc8d001177fadc593a745adad6d867f106
lgrill-pentaho <larry.grill@hitachivantara.com>
2024-09-24 18:11:12 -0400

    Revert "HNC-766: updated license and copyright headers"
    This reverts commit 1a1b3650128e58377b954469ca9a6a25e871f856.

--- pentaho/src/main/java/pt/webdetails/cpf/repository/pentaho/SystemPluginRWAccess.java
+++ pentaho/src/main/java/pt/webdetails/cpf/repository/pentaho/SystemPluginRWAccess.java
@@ -1,14 +1,3 @@
-/*! ******************************************************************************
- *
- * Pentaho
- *
- * Copyright (C) 2024 by Hitachi Vantara, LLC : http://www.pentaho.com
- *
- * Use of this software is governed by the Business Source License included
- * in the LICENSE.TXT file.
- *
- * Change Date: 2028-08-13
- ******************************************************************************/
 package pt.webdetails.cpf.repository.pentaho;
 
 import pt.webdetails.cpf.repository.api.IRWAccess;
